9cb1644e56539c4bea696296719f816847ecdf6d
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2021-03-08 19:59:33 -0300

    meli 8-3
    pequeños cambios en le nav de perfil que no llegaron a nada

---
+++ controller/user/controller_user.js
@@ -0,0 +1,14 @@
+$(function(){
+    console.log('ControlerUser');
+    
+    function clickDatos(){
+      var isSomethingTrue = true;
+        
+        if(isSomethingTrue){
+          alert("something is true");
+        }else{
+          alert("something is false");
+        }
+    });
+});
+
--- view/shared/User.php
+++ view/shared/User.php
@@ -1,13 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Simple Sidebar - Start Bootstrap Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="../asset/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="../asset/css/style_login.css" rel="stylesheet">
+  <link href="../asset/css/perfil.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <div class="d-flex" id="wrapper">
+
     <!-- Sidebar -->
     <div class="border-right" id="sidebar-wrapper">
       <div class="sidebar-heading">LOGO</div>
       <div class="list-group list-group-flush">
-        <a href="#" class="list-group-item list-group-item-action ">
+        <a href="#" class="list-group-item list-group-item-action">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
             </svg>
             Perfil
         </a>
+        <div class="submenu_personal">
+        <ul>
+        <li>
+        <a href="#" id="datosPersonales" onclick="clickDatos()">Datos personales </a>
+        </li>
+        </ul>
+        </div>
+
+        
         <a href="#" class="list-group-item list-group-item-action">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
@@ -33,3 +67,44 @@
     </div>
     <!-- /#sidebar-wrapper -->
 
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+
+      <nav class="navbar navbar-expand-lg border-bottom">
+        <button class="btn" id="menu-toggle">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+        </svg>
+        </button>
+
+        
+      </nav>
+
+      <div class="herramienta">
+      </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+
+  </div>
+  <!-- /#wrapper -->
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="../asset/jquery/jquery.min.js"></script>
+  <script src="../asset/js/bootstrap/bootstrap.js"></script>
+  <script src="../asset/js/bootstrap/bootstrap.bundle.min.js"></script>
+  <script src="..\..\controller\user\controller_user.js" type="module" ></script>
+
+  <!-- Menu Toggle Script -->
+  <script>
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+
+
+
+  </script>
+
+</body>
+
+</html>
