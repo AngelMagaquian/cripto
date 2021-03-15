dcdb9b2887f615ced5f3a6919e152e0acdb75524
AngelMagaquian <69281069+AngelMagaquian@users.noreply.github.com>
2021-03-15 19:57:43 -0300

    cambios en user

--- view/shared/User.php
+++ view/shared/User.php
@@ -119,6 +119,7 @@
   <script src="../asset/js/bootstrap/bootstrap.bundle.min.js"></script>
   <script src="..\..\controller\user\controller_user.js" type="module" ></script>
   <script src="..\..\controller\user\update_user_info_controller.js" type="module" ></script>
+  <script src="..\..\controller\datos_bancarios\bank_controller.js" type="module" ></script>
 
   
 
