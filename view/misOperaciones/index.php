2ed4f03ef20577654560bc0944885f1c26770d18
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2021-03-22 22:59:14 -0300

    meli_22-03
    se acomodaron los filtros de forma responsive (falta)

--- view/misOperaciones/index.php
+++ view/misOperaciones/index.php
@@ -6,42 +6,56 @@
                 <div class="card">
                     <div class="card-body mt-3 ">
                     
-                        <div class="row justify-content-around ">
-                            <form id="date_form" >
-                                <div class="col-2">
-                                    <label for=""><b>Desde</b></label>
-                                    <input id="desde" type="date" class="form-control" value='<?php echo date("Y-m-d");?>'>
+                        <div class="row justify-content-around">
+                            <form id="date_form" class="col-4">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <label for=""><b>Desde</b></label>
+                                        <input id="desde" type="date" class="form-control" value='<?php echo date("Y-m-d");?>'>
+                                    </div>
+                                    <div class="col-6">
+                                        <label for=""><b>Hasta</b></label>
+                                        <input id="hasta" type="date" class="form-control" value='<?php echo date("Y-m-d");?>'>
+                                    </div>
+                                    <div class="col-4 mt-3">
+                                        <input type="submit" value="Filtro de fecha" class="btn-get-started-filtro btn-filtro"/>
+                                    </div>
+                                    
                                 </div>
-                                <div class="col-2">
-                                    <label for=""><b>Hasta</b></label>
-                                    <input id="hasta" type="date" class="form-control" value='<?php echo date("Y-m-d");?>'>
-                                </div>
-                                <input type="submit" value="Filtro de fecha"/>
                             </form>
-                            <form id="operation_form">
-                                <div class="col-2">
-                                    <label for=""><b>Operación</b></label>
+                            <form id="operation_form " class="col-2">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <label for=""><b>Operación</b></label>
                                         <br>
+                                        <div>
+                                            
+                                        </div>
                                         <input type="checkbox" id="op1" name="op1" value="compra">
-                                        <label for="op1"> Compra</label><br>
-                                      
-                                        <input type="checkbox" id="op2" name="op2" value="venta">
-                                        <label for="op2"> Venta</label><br>
+                                        <label for="op1"> Compra</label>
                                         
-                                    <input type="submit" value="Filtro de operación"/>
+                                        <input type="checkbox" id="op2" name="op2" value="venta" class="ml-3">
+                                        <label for="op2"> Venta</label><br>    
+                                    </div>
+                                    <div class="col-4 mt-3">  
+                                        <input type="submit" value="Filtro de operación" class="btn-get-started-filtro btn-filtro"/>
+                                    </div>
                                 </div>
                             </form>
-                            <form id="cripto_form">
-                                <div class="col-2">
+                            <form id="cripto_form" class="col-2">
+                            <div class="row">
+                                <div class="col-12">
                                     <label for=""><b>Divisa</b></label>
                                     <select name="" id="divisa" class="form-select">
                                     
                                     </select>
-                                    <input type="submit" value="Filtro de divisa"/>
+                                    
                                 </div>
-                                <div class="col-2 align-self-center">
-                                    <a class="btn-get-started-filtro btn-filtro">Aplicar filtro</a>
+                                <div class="col-4 align-self-center">
+                                    <input type="submit" value="Filtro de divisa" class="btn-get-started-filtro btn-filtro"/>
                                 </div>
+                            </div>
+                               
                             </form>
                         </div>
                     
