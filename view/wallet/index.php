a83e503f330205c59eee9a0ad72066fe6a5a2b5c
AngelMagaquian <69281069+AngelMagaquian@users.noreply.github.com>
2021-03-26 19:48:27 -0300

    wallet change
    Signed-off-by: AngelMagaquian <69281069+AngelMagaquian@users.noreply.github.com>

--- view/wallet/index.php
+++ view/wallet/index.php
@@ -1,59 +1,58 @@
 
 <section id="wallet" style="display:none">
 
-    <div class="container-fluid">
-        <div class="row justify-content-center mt-4">
+<div class="container-fluid">
+    <div class="row justify-content-center mt-4">
 
-            <div class="col-11">   
+        <div class="col-11">   
 
-                <div class="card">
-                    <div class="card-body mt-3">
-                    <div class="row justify-content-between">
-                        <h5>Mis wallet</h5>
-                        <form id="" class="col-lg-4 col-md-6 col-sm-12">
-                            <div class="row">
-                                <div class="col-md-6 col-sm-6 col-8">
-                                    <input type="text" placeholder="Ingresar código de wallet" class="form-control">
-                                </div>
-                                <div class="col-2">
-                                    <input type="submit" value="Buscar" class="btn-get-started-filtro btn-filtro">
-                                </div>
-                                    
+            <div class="card">
+                <div class="card-body mt-3">
+                <div class="row justify-content-between">
+                    <h5>Mis wallet</h5>
+                    <form id="" class="col-lg-4 col-md-6 col-sm-12">
+                        <div class="row">
+                            <div class="col-md-6 col-sm-6 col-8">
+                                <input type="text" placeholder="Ingresar código de wallet" class="form-control">
                             </div>
-                        </form>
-                        <form id="" class="col-lg-4 col-md-6 col-sm-12 mt-md-0 mt-sm-4 mt-4">
-                            <div class="row">
-                                <div class="col-12" style="text-align: end;">
-                                    <input type="submit" value="Nueva wallet" class="btn-get-started-wallet btn-wallet">
-                                </div>
+                            <div class="col-2">
+                                <input type="submit" value="Buscar" class="btn-get-started-filtro btn-filtro">
                             </div>
-                        </form>   
-                    </div>
-
-                    <div class="row mt-4">
-                        <table id="walletTable" class="table table-striped table-bordered" style="width:100%">
-                            <thead>
-                                <tr>
-                                    <th>Id wallet</th>
-                                    <th>Tipo de divisa</th>                
-                                </tr>
-                            </thead>
-                            <tbody id="operation_tbody">
                                 
-                            </tbody>
-                        </table>
                         </div>
-                        
-                                 
-                        
-                    </div>
+                    </form>
+                    <form id="" class="col-lg-4 col-md-6 col-sm-12 mt-md-0 mt-sm-4 mt-4">
+                        <div class="row">
+                            <div class="col-12" style="text-align: end;">
+                                <input type="submit" value="Nueva wallet" class="btn-get-started-wallet btn-wallet">
+                            </div>
+                        </div>
+                    </form>   
                 </div>
 
+                <div class="row mt-4">
+                    <table id="walletTable" class="table table-striped table-bordered" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Id wallet</th>
+                                <th>Tipo de divisa</th>                
+                            </tr>
+                        </thead>
+                        <tbody id="wallet_tbody">
+                            
+                        </tbody>
+                    </table>
+                    </div>
+                    
+                             
+                    
+                </div>
             </div>
 
         </div>
+
     </div>
+</div>
 
 </section>
 
- 
