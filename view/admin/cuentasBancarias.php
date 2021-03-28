3f77c5e4207217520a92a7975461c69fefae243b
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2021-03-28 19:57:00 -0300

    27-03
    Se crearon vistas de admin

---
+++ view/admin/cuentasBancarias.php
@@ -0,0 +1,93 @@
+<section id="cuentas" style="display:none">
+    <div class="container-fluid">
+        <div class="row justify-content-center mt-4">
+          <div class="col-lg-10 col-12">
+            <div class="row">
+                <h5>Confirme la cuenta de banco asociada al usuario</h5>
+
+                <div class="col-6">
+                    <form action="">
+                        <span>Buscar usuario por DNI</span>
+                        <input type="text" value=""> 
+                        <div class="card">
+                            <div class="card-body row justify-content-around">
+                                <span>Datos del usuario</span>
+                                <div class="col-md-5 col-12 mt-">
+                                    <label><b>Nombre</b></label>
+                                    <input type="text" class="form-control form-login"  value="valor de prueba"id="name" disabled=»disabled»>
+                                </div>
+                                <div class="col-md-5 col-12 mt-2">
+                                    <label><b>Segundo nombre</b></label>
+                                    <input type="text" class="form-control form-login" id="CUIL" disabled=»disabled»>
+                                </div>
+                                <div class="col-md-5 col-12 mt-2">
+                                    <label><b>Apellido</b></label>
+                                    <input type="text" class="form-control form-login" id="type_account" disabled=»disabled»>
+                                </div>
+                                <div class="col-md-5 col-12 mt-2">
+                                    <label><b>Segundo apellido</b></label>
+                                    <input type="text" class="form-control form-login" id="type_currency" disabled=»disabled»>
+                                </div>
+                                <div class="col-md-5 col-12 mt-2">
+                                    <label><b>DNI</b></label>
+                                    <input type="text" class="form-control form-login" id="CBU" disabled=»disabled»>
+                                </div>
+                                <div class="col-md-5 col-12 mt-2">
+                                    <label><b>CUIL</b></label>
+                                    <input type="text" class="form-control form-login" id="account_number" disabled=»disabled»>
+                                </div>
+                                <div class=" col-11 mt-2">
+                                    <label><b>Email</b></label>
+                                    <input type="text" class="form-control form-login" id="account_number" disabled=»disabled»>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="col-6">
+                    <form action="">
+                    <span>Cuentas de usuario</span>
+                        <div class="card">
+                            <div class="card-body row">
+                                <div class="col-12">
+                                    <table id="cuentasTable" class="table table-striped table-bordered" style="width:100%">
+                                        <thead>
+                                            <tr>
+                                                <th>Banco</th>
+                                                <th>CBU</th>
+                                                <th></th>
+
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>-</td>
+                                                <td>-</td>
+                                                <td>
+                                                    <button class="btn btn-success">si</button>
+                                                    <button class="btn btn-danger">no</button>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
+              
+            </div>
+            </div>
+            
+          </div>
+
+          </div>
+        </div>
+
+        
+    </div>
+
+</section>
