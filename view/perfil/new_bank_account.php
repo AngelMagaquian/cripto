8ecfb7a413dfde97aefd06aaaa79ba3e5e49ff77
AngelMagaquian <69281069+AngelMagaquian@users.noreply.github.com>
2021-04-13 00:14:58 -0300

    new bank account complete

--- view/perfil/new_bank_account.php
+++ view/perfil/new_bank_account.php
@@ -1,65 +1,48 @@
-<section id="new_bank_section">
+<script src="..\..\controller\datos_bancarios\new_account_controller.js" type="module" ></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script> <!--late delete script-->
+
+<section id="bancos" >
     <div class="container-fluid">
         <div class="row justify-content-center mt-4">
           <div class="col-lg-10 col-12">
             <div class="row">
-                <div class="col-md-6 col-12 mt-md-0 mt-4">
-                    <div class="card">
-                        <div class="card-header">
-                            <span id="new_bank">Agregar nueva cuenta bancaria</span>
-                            
-                        </div>
-                        <div class="card-body row justify-content-around">
-                            <div class="col-md-5 col-12 mt-">
-                                <label><b>Seleccione un banco</b></label>
-                                <select class="form-control form-login" id="new_bank_select"></select>
-                            </div>
-                            <div class="col-md-5 col-12 mt-2">
-                                <label><b>CUIL</b></label>
-                                <input type="text" class="form-control form-login" id="new_bank_CUIL" autocomplete="off" requiered>
-                            </div>
-
-                            <div class="col-md-5 col-12 mt-2">
-                                <label><b>Tipo de cuenta</b></label>
-                                <select class="form-control form-login" id="new_type_account"> </select>
-                            </div>
-                            <div class="col-md-5 col-12 mt-2">
-                                <label><b>Tipo de moneda</b></label>
-                                <select class="form-control form-login" id="new_type_currency">
-                                    <option value="1">PESOS</option>
-                                    <option value="2">DOLARES</option>
-                                </select>
-                            </div>
-
-                            <div class="col-md-5 col-12 mt-2">
-                                <label><b>CBU</b></label>
-                                <input type="text" class="form-control form-login" id="new_CBU" autocomplete="off" requiered>
-                            </div>
-                            <div class="col-md-5 col-12 mt-2">
-                                <label><b>Nro de cuenta</b></label>
-                                <input type="text" class="form-control form-login" id="new_account_number" autocomplete="off" requiered>
-                            </div>
-                        </div>
+                <div class="col-md-6 col-12" >
+                    <form id="new_account_user">
+                    <div class="" >
+                        <label>Seleccionar Banco</label>
+                        <Select id="select_bank"> 
+                            <!--Option de bancos-->
+                        </Select>
                     </div>
 
-                </div>
-            </div>
-            <div class="row mt-5">
-                <div class="col-12 botonera">
-                    <form id="insert_new_bank">
-                        <input type="submit" value = "Agregar" class="btn-get-started-agg  btn-agg mr-2"/>
-                    </form>
-                    
-                </div>
-              
-            </div>
-              
-            </div>
-            </div>
-            
-          </div>
+                    <div class="" >
+                        <label>Seleccionar Tipo de cuenta</label>
+                        <Select id="select_type_account"> 
+                            <!--Option de cuentas-->
+                        </Select>
+                    </div>
 
-          </div>
+                    <div class="">
+                        <label>CBU</label>
+                        <input type="text" placeholder="Ingrese su CBU" class="" id="new_cbu" autocomplete="off" pattern="[0-9]+" required />
+                    </div>
+
+                    <div class="">
+                        <label>Alias</label>
+                        <input type="text" placeholder="Ingrese su alias" class="" id="alias" autocomplete="off"  required />
+                    </div>
+
+                    <div class="">
+                        <label>Número de cuenta</label>
+                        <input type="text" placeholder="Ingrese su número de cuenta" class="" id="new_num_acc" autocomplete="off" pattern="[0-9]+" required />
+                    </div>
+                        <input type="submit" value="Agregar" class=""/>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </section>
+
+
+
