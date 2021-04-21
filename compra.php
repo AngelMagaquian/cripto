4e27d03d12aa703bb40622069fab25f525673298
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2021-04-21 19:28:01 -0300

    MELI 21-4
    Cambios en los textos del home

--- compra.php
+++ compra.php
@@ -222,23 +222,45 @@ include 'nav.php';
     <div class="row justify-content-sm-between justify-content-center">
       <div class="col-md-4 col-sm-6 col-11 mb-md-0 mb-sm-4 mb-5">
         <div class="card-tips">
+          <div class="p-3">
+            <div class=" container-svg">
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-check-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+                <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              </svg>
+            </div>
+          </div>
           <h3><b>Registrate</b></h3>
-          <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has</p>
+          <p>Abrí tu cuenta virtual y entra en la nueva economia.</p>
         </div>
         
       </div>
 
       <div class="col-md-4 col-sm-6 col-11  mb-md-0 mb-sm-4 mb-5">
         <div class="card-tips">
+          <div class="p-3">
+            <div class=" container-svg">
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+              </svg>
+            </div>
+          </div>
           <h3><b>Operá</b></h3>
-          <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has</p>
+          <p>Te ayudamos a abrir tu primera caja de ahorros virtual.</p>        
         </div>
       </div>
 
       <div class="col-md-4 col-sm-6 col-11">
         <div class="card-tips">
+        <div class="p-3">
+            <div class=" container-svg">
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#fff" class="bi bi-graph-up" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5z"/>
+              </svg>
+            </div>
+          </div>
         <h3><b>Vé tus ingresos crecer</b></h3>
-          <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has</p>
+          <p>Compra y vende todas las criptomonedas sin comisiones.</p>
         </div>
       </div>
 
@@ -254,7 +276,7 @@ include 'nav.php';
 
         <div class="col-md-5 col-11 text-random">
             <h5><b>TE ASESORAMOS EN LO QUE NECESITES</b></h5>
-            <p>Ahora operar en criptomonedas es mas facil… Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.</p>
+            <p>Ahora operar en <b>criptomonedas</b> es más facil. Te ofrecemos atención y asesoramiento personalizado, te ayudamos a abrir tu primera caja de ahorros virtual y te asesoramos si necesitas enviar o recibir dinero desde el exterior</p>
         </div>
 
         <div class="col-md-5 col-11 mt-md-0 mt-5">
